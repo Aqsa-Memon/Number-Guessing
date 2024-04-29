cb39a5cb4575401168b4fd8a5b9a95dba5982537
Aqsa-Memon <muhammadzaidjati2@gmail.com>
2024-04-29 17:13:58 +0500

    This Is CLI Based Numbers Guessing Game

---
+++ app.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ramdom_Number = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt({
+    name: "guessedNumber",
+    type: "number",
+    message: "guess a number between 1-5: ",
+});
+if (answer.guessedNumber === ramdom_Number) {
+    console.log("\ncongratulation! you guessed correctly!\n");
+}
+else {
+    console.log("\nYou guessed wrong number\n ");
+}
